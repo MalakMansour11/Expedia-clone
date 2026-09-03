4ba4643a532b30876bffdccc288181338adc8d12
Malak <malak.mansour.0405@gmail.com>
2026-09-03 12:48:38 -0500

    Fix Firebase configuration and GitHub login

--- src/01_firebase/config_firebase.js
+++ src/01_firebase/config_firebase.js
@@ -1,20 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlKHCdVfQvMRKQf2B5UqaLfDkwm6T1apo",
-  authDomain: "challo-ghume-fa22c.firebaseapp.com",
-  projectId: "challo-ghume-fa22c",
-  storageBucket: "challo-ghume-fa22c.appspot.com",
-  messagingSenderId: "411994731743",
-  appId: "1:411994731743:web:a02cec85eb6aa66ddbe56a",
-  measurementId: "G-5KYMV9KZ2D"
+  apiKey: "AIzaSyClA06_1rvT3X3yu8bg2mFyzmbTLFQnL7A",
+  authDomain: "expedia-clone-malak-mansour.firebaseapp.com",
+  projectId: "expedia-clone-malak-mansour",
+  storageBucket: "expedia-clone-malak-mansour.firebasestorage.app",
+  messagingSenderId: "938607109157",
+  appId: "1:938607109157:web:1a3bf859870d578e088bcc",
+  measurementId: "G-GK3K06HR5N"
 };
 
 // Initialize Firebase
 const firebase_app = initializeApp(firebaseConfig);
 
-export default firebase_app
+export default firebase_app;
